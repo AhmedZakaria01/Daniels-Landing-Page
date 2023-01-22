1e197f0133c60fb75259df626892ff45b3df9715
Ahmed Zakaria <ahmed.zakaria.sera@gmail.com>
2023-01-22 23:28:56 +0200

    Project

--- js/script.js
+++ js/script.js
@@ -1,110 +1,33 @@
 $(document).ready(function(){
+  $(".load .spinner").fadeOut(1500, ()=> {
+    $(".load").fadeOut(400, ()=> {
+        $("body").css("overflow","auto");
+        $(".load").remove()
+    })
+})
+// #
+  $("#menuBtn i ").click(()=> {
+    $(".sideBarMain").animate($(".sideBarMain").toggleClass("open"))
+  })
+// #
    $(".owl-carousel").owlCarousel({
     items : 3,
     dots: true,
    });
-
-   let aboutMeOffset = $(".about-me").offset().top;
-  //  console.log(aboutMeOffset);
+// #
+     $(".skitter-large").skitter({
+      responsive: true,
+      interval: 2000,
+      show_randomly: true
+     });
+// #
+   let skitterOffset = $(".skitter").offset().top;
    $(window).scroll(function () {
-    if ($(window).scrollTop() > aboutMeOffset -70 ) {
+    if ($(window).scrollTop() > skitterOffset -200 ) {
       $('.navbar').css('background','rgba(0,0,0,9)');
-      $(".navbar").animate( {paddingRight:100,paddingLeft:100},1300 )
+      $(".navbar").animate( {paddingTop:-50,paddingBottom:-50},1300 )
     }else {
       $('.navbar').css('background','transparent');
     }
   });
-// ------------------
-  $(".loading .spinner").fadeOut(100, ()=> {
-    $(".loading").fadeOut(100, ()=> {
-        $("body").css("overflow","auto")
-    })
-})
-// ------------------
-
-
-$("#sideBar i").click(()=> {
-  let boxWidth = $("#box").outerWidth()
-  if ($("#sideBar").css('left') === "0px") {
-    boxWidth =  $("#sideBar").animate({left: -`${boxWidth}`},1000)
-  }else {
-    boxWidth =  $("#sideBar").animate({left: 0},1000)
-  }
-})
-
-let colorSpans = $("#sideBar span");
-for (let i = 0; i < colorSpans.length; i++) {
-  let red =  Math.floor( Math.random() * 255 );
-  let green =  Math.floor( Math.random() * 255 );
-  let blue =  Math.floor( Math.random() * 255 );
-  $("#sideBar span").eq(i).css("background-color", `rgb(${red}, ${green}, ${blue}) `)
-}
-
-colorSpans.click((e)=> {
-  targetColor = $(e.target).css("background-color")
-  $(".change").css("color", targetColor)
-})
-
-
-
-
-
-
-
-
-
-
  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  let aboutMeOffset = $(".about-me").offset().top;
-//  console.log(aboutMeOffset);
-
-//  $(window).scroll(function () {
-//   if ($(window).scrollTop() > aboutMeOffset -70 ) {
-//     $('.navbar').css('background','rgba(0,0,0,9)');
-//     $(".navbar").animate( {paddingRight:100,paddingLeft:100},1300 )
-//     // $(".navbar-toggler").css("color", "white")
-//     // $(".navbar-toggler-icon").addClass("text-danger");
-
-      
-//   }else {
-//     $('.navbar').css('background','transparent');
-//   }
-// });
-
-
-
-
-
-$(document).ready(function () {
-    // $(".loading .spinner").fadeOut(100, ()=> {
-    //     $(".loading").fadeOut(100, ()=> {
-    //         $("body").css("overflow","auto")
-
-    //     })
-    // })
-});
-
